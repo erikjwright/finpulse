d096c77935a782a919a140abaaa241a4ffa77948
Erik Wright <erik@modo.build>
2024-10-29 11:39:59 +0100

    update use production url env vars

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -15,8 +15,8 @@ import { TransactionsTable } from "../../components/transactions/table";
 
 export default async function Dashboard() {
 	const client = hc<AppType>(
-		process.env.NEXT_PUBLIC_VERCEL_URL
-			? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+		process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+			? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
 			: "http://localhost:3000",
 	);
 
